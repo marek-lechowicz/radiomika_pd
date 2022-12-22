2ee8eef1e0c4ca06e35f40256988188d32a15c73
Marek Lechowicz <m.lechow13@gmail.com>
2022-12-22 02:09:20 +0100

    tree classifier

--- convert_to_gray.py
+++ convert_to_gray.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import SimpleITK as sitk
 import os
+import numpy as np
 read_path = r'./raw_data/lgg-mri-segmentation/kaggle_3m'
 write_path = r'./data'
 arr = os.listdir(read_path)
@@ -12,9 +13,15 @@ for folder in arr:
         continue
     arr_files = os.listdir(folder_path)
     for file in arr_files:
-        # filname = file.split('.')[0]
-        # mask_filename = filname + "_mask"
-        # if 
+        filname = file.split('.')[0]
+        if "mask" not in filname:
+            mask_filename = filname + "_mask"
+        else: 
+            mask_filename = filname
+        mask = cv2.imread(folder_path  + "/" + mask_filename + ".tif")
+        if np.all(np.unique(mask) == 0):
+            continue
+        print(filname)
         read_file_path = folder_path + "/" + file
         img = cv2.imread(read_file_path)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
--- extract_features.py
+++ extract_features.py
@@ -6,44 +6,66 @@ import pandas as pd
 import SimpleITK as sitk
 from radiomics import featureextractor
 
+# set path for image data
 data_path = r'./data'
 data_files = os.listdir(data_path)
 
-# features data frame
+# get clinical data
+clinical = pd.read_csv(r'./raw_data/data.csv')
+
+# define "features" DataFrame
 features = None
 
 # Instantiate the extractor
 settings = {'label': 255, 'force2D': True}
 extractor = featureextractor.RadiomicsFeatureExtractor(**settings)
+extractor.enableFeatureClassByName('shape2D')
+extractor.enableAllImageTypes()
 
 print('Extraction parameters:\n\t', extractor.settings)
 print('Enabled filters:\n\t', extractor.enabledImagetypes)
 print('Enabled features:\n\t', extractor.enabledFeatures)
-i = 0
+
 for file in data_files:
-    
-    if i > 10:
-        break
+    # Check if file is a mask file 
     filename = file.split('.')[0]
+    # if yes - skip it
     if "mask" in filename:
         continue
+    # if not - get mask file
     else: 
         mask_filename = filename + "_mask"
 
-    print(f"Processing: {file}")
-    i += 1
     mask_path = data_path  + "/" + mask_filename + ".tif"
     img_path = data_path  + "/" + filename + ".tif"
 
+    # inform which file is currently processed (to track the progress)
+    print(f"Processing: {file}")
+
+    # execute the extraction
     result = extractor.execute(img_path, mask_path)
-    to_save = dict()
+
+    # get patient name
+    arr_filename = filename.split('_')
+    patient_name = arr_filename[0] + "_" + arr_filename[1] + "_" + arr_filename[2]
+    
+    # get information if cancer was malign
+    malign = clinical[clinical['Patient'] == patient_name]['death01'].values[0]
+    
+    # define information to be saved as one row
+    to_save = {'patient_name': patient_name, 'malign': malign}
+
+    # get only features from "result" dictionary
     for key, value in result.items():
         if any(feature_type in key for feature_type in extractor.enabledFeatures.keys()):
             to_save[key] = value
-    if features is None:
-        features = pd.DataFrame([result])
-    else:
-        features = features.append(result, ignore_index=True)
 
+    # append extracted features to "fetures" DataFrame
+    if features is None:
+        features = pd.DataFrame([to_save])
+    else:
+        features = features.append(to_save, ignore_index=True)
+
+# save DataFrame to .csv file
 features.to_csv("features.csv")
        
